c457b09a5fa41473b5ca12b684575be95d4cc35c
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2022-11-15 22:54:38 +0100

    z2

--- src/test/java/ba/unsa/etf/rpr/FiksniBrojTest.java
+++ src/test/java/ba/unsa/etf/rpr/FiksniBrojTest.java
@@ -3,8 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FiksniBrojTest {
+    FiksniBroj.Grad grad1 = FiksniBroj.Grad.valueOf("BUGOJNO");
+    String broj = "022-333";
+    FiksniBroj f1 = new FiksniBroj(grad1,broj);
 @Test
     public void ispisGrada(){
-    assertTrue( true );
+    assertEquals(f1.ispisi(),"Grad{030}022-333");
 }
 }
