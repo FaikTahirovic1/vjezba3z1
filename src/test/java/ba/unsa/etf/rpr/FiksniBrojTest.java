ab0d6f86db5e8d34c87da40c775cf7cb9948b8e7
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2022-11-15 22:15:55 +0100

    z2

---
+++ src/test/java/ba/unsa/etf/rpr/FiksniBrojTest.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FiksniBrojTest {
+@Test
+    public void ispisGrada(){
+    assertTrue( true );
+}
+}
